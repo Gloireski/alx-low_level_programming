9bf383dfebba76af875f155db08a30feea00756e
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-09 14:52:56 -0800

    beety

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
