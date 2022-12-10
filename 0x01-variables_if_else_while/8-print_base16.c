762e8435954f94bd613afd2f1e612a22cf26e365
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-10 14:40:35 -0800

    task 9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,37 +7,16 @@
 int main(void)
 {
 		int i;
+		char ch = 'a';
 
-		for (i = 1; i <= 16; i++)
+		for (i = 1; i <= 10; i++)
 		{
-			if (i == 10)
-			{
-				putchar('a');
-			}
-			else if (i == 11)
-			{
-				putchar('b');
-			}
-			else if (i == 12)
-			{
-				putchar('c');
-			}
-			else if (i == 13)
-			{
-				putchar('d');
-			}
-			else if (i == 14)
-			{
-				putchar('e');
-			}
-			else if (i == 15)
-			{
-				putchar('f');
-			}
-			else
-			{
-				putchar(i % 10 + '0');
-			}
+			putchar(i % 10 + '0');
+		}
+		while (ch <= 'f')
+		{
+			putchar(ch);
+			ch++;
 		}
 		putchar('\n');
 		return (0);
