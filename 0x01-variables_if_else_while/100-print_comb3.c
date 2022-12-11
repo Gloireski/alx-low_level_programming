ea3c16c2bdb70926a53b43d274754d3e15639037
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-10 16:34:38 -0800

    task 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
 		for (j = 1; j < 10; j++)
 		{
@@ -17,7 +17,6 @@ int main(void)
 			{
 				putchar(i % 10 + '0');
 				putchar(j % 10 + '0');
-				if (j != 9)
 				{
 					putchar(',');
 					putchar(' ');
