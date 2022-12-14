bd909d3a8e2e13d4189ce02cafc978cedac009aa
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-13 16:06:26 -0800

    t1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* more headers goes there */
+#include "_putchar.c"
+/* betty style doc for function main goes there */
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
