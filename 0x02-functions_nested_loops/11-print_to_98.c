4e6be48a6553daa5b7137ce7b2d79a9bb184e867
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-14 18:45:29 -0800

    task 12

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: The integer param.
+ * Return: ...
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n; n >= 98; n--)
+			_putchar(n + '0');
+	}
+	else
+	{
+		for (n; n <= 98; n++)
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
