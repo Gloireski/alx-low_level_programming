3491068e107dc94094a62b9a4fac32689556f597
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-15 09:12:54 -0800

    fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: 0;
+ */
+int main(void)
+{
+	int n, z = 0;
+
+	for (n = 1; n < 50; n++)
+	{
+		if (n <= 2)
+		{
+			printf("%d, ", n);
+		}
+		else if (n == ((n - 1) + (n - 2)))
+		{
+			printf("%d, ", n);
+		}
+	}
+	return (0);
+}
