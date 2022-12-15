a2765a1e2d38727aa39cc2d9ddbdabddd3716df4
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-14 19:03:40 -0800

    task 12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,21 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
 		while (n >= 98)
 		{
-			_putchar(n + '0');
+			if(n == 98)
+			{
+				_putchar(n + '0');
+				break;
+			}
+			else
+			{
+				_putchar(n +'0');
+				_putchar(44);
+				_putchar(32);
+			}
 			n--;
 		}
 	}
@@ -18,7 +28,17 @@ void print_to_98(int n)
 	{
 		while(n <= 98)
 		{
-			_putchar(n + '0');
+			if (n == 98)
+			{
+				_putchar(n + '0');
+				break;
+			}
+			else
+			{
+				_putchar(n + '0');
+				_putchar(44);
+				_putchar(32);
+			}
 			n++;
 		}
 	}
