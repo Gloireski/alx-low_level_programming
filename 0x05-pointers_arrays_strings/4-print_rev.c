d60fa150f9feed64ba185407aa43faf258b72ab6
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-20 06:33:56 -0800

    task3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: param
+ * Return: ...
+ */
+void _puts(char *str)
+{
+	int c = 0;
+	
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	putchar('\n');
+}
