2ca9aedfca6f44ffe2f8e0e0d210a7bda64c94dc
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-19 18:21:50 -0800

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,16 @@
 /**
  * _strlen - returns the length of a string
  * @s : param
- * Return : length of s
+ * Return : lenght of s
  */
 
 int _strlen(char *s)
 {
 	int c = 0;
 
-	for (; *s != '\n'; s++)
+	for (; *s != '/0'; s++)
+	{
 		c++;
-
+	}
 	return (c);
 }
