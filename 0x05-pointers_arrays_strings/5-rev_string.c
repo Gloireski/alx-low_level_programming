190763cee08e7f73bbf2212059347baa1b07ef48
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-20 12:43:29 -0800

    task4

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,23 @@
  */
 void rev_string(char *s)
 {
-	int c = 0, d = 0;
-	char *tmps;
+	int c = 0, d, e;
+	char *tmps, chg;
 
 	tmps = s;
 	while (s[c] != '\0')
 	{
 		c++;
 	}
-	for (c -= 1; c >= 0; c--)
+	for (d = 1; d < c; d++)
 	{
-		tmps[d] = s[c];
-		d++;
+		tmps++;
 	}
-
-	*s = *tmps;
+	for (e = 0; e < (c / 2); e++)
+	{
+		chg = s[e];
+		s[e] = *tmps;
+		*tmps = cg;
+		tmps--;
+	}
 }
