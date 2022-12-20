3cd9d6fe7f697d9c9fb7db8857a1bcfcad6fc884
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-20 13:56:49 -0800

    task6

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,15 @@ void puts_half(char *str)
 	{
 		length_of_the_string++;
 	}
-	for (i = (length_of_the_string / 2); i <= length_of_the_string-1; i++)
+	if ((length_of_the_string % 2) == 0)
 	{
-		_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (length_of_the_string / 2); i <= length_of_the_string-1; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
