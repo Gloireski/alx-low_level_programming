25e829a31bfa43e6c2e6866bd063d4589410446f
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-20 11:48:00 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: param
+ * Return: ...
+ */
+void rev_string(char *s)
+{
+	int c = 0, d = 0;
+	char *tmp;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		tmp[d] = s[c];
+		d++;
+	}
+
+	*s = *tmp;
+}
