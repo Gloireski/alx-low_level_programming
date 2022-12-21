a1674b1c31dd829c5fc08082fb4e6efff5a6a5a7
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-20 16:37:33 -0800

    task7

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,18 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
-	while (src[i] != '\0')
+	for (i; src[i] != '\0'; i++)
 	{
+		dest[i] = src[i];
 		i++;
 	}
-
-	while (j <= i + 1)
-	{
-		dest[j] = src[j];
-		j++;
-	}
+	dest[i++] = '\0';
 
 	return (dest);
 }
