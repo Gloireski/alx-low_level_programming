793c08b06ccbc563ca4337dc30e0dd89cbf42a08
Gloireski <belemgloirebekoutou8@gmail.com>
2022-12-22 16:58:33 -0800

    ./6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string -  capitalizes all words of a string.
+ * @str: string
+ * Return: new string pointer
+ */
+
+char *cap_string(char *str)
+{
+	int c =0, i, tspc = 13;
+	char spc[] = {32, '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')',
+	'{', '}'};
+
+	while (str[c])
+	{
+		i = 0;
+		while (i < tspc)
+		{
+			if ((c == 0 || str[c - 1] == spc[i]) && (str[c] >= 97 && str[c] <= 122))
+			{
+				str[c] = str[c] - 32;
+			}
+			i++;
+		}
+		c++;
+	}
+
+	return (str);
+}
