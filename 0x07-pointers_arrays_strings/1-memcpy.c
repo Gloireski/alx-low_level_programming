09be113a66b42bc8830f58c30da2ce19ce89b2af
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-02 17:38:32 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset -  fills memory with a constant byte.
+ * _memcpy -  fills memory with a constant byte.
  * @dest: pointer to string
  * @src: byte
  * @n: integer
