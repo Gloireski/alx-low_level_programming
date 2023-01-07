d34230cd2bef166c793e28e848e6e31afc892648
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-07 02:50:57 +0100

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,8 +1,47 @@
 #include "main.h"
 
 /**
-  */
+ * _strcmp - compare two strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 if the strings are same
+ */
+
 int _strcmp(char *s1, char *s2)
 {
+	int i, cmp = 0, stp = 0;
+	int c = 0, d = 0;
 
+	while (s1[c])
+	{
+		c++;
+	}
+
+	while (s2[d])
+	{
+		d++;
+	}
+
+	if (c > d)
+	{
+		stp = c;
+	}
+	else
+	{
+		stp = d;
+	}
+	for (i = 0; i < stp; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			continue;
+		}
+		else
+		{
+			cmp = s1[i] - s2[i];
+			break;
+		}
+	}
+
+	return (cmp);
 }
