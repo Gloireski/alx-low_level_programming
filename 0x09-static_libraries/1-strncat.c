70cddaf500e5d2af5540a3024e8d6813d5556324
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-07 02:38:38 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,8 +1,29 @@
 #include "main.h"
 
 /**
-  */
-char *_strncat(char *dest, char *src)
-{
+ * _strncat - concatenates two strings.
+ * @dest:destination
+ * @src: sourcei
+ * @n: size
+ * Return: the pointer to dest.
+ */
 
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, c = 0;
+
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[c] = src[i];
+		c++;
+	}
+
+	if (i <= n)
+		dest[c] = '\0';
+
+	return (dest);
 }
