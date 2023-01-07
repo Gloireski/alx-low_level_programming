5524d2c2643a5645f2dfa3b66578a900f741c85c
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-07 02:37:54 +0100

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,8 +1,21 @@
 #include "main.h"
 
 /**
-  */
+ * _memcpy -  copy memory area.
+ * @dest: destination string
+ * @src: source string
+ * @n: integer
+ * Return: pointer to the memory area s.
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
