45571de6511ab8cc65de1a70f139897e47225ee5
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-07 02:49:01 +0100

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,8 +1,15 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
-  */
+ * _islower - checks for lowercase character
+ * @c: The character to check
+ * Return: if c is lowercase 1.
+ * Otherwise, 0 is returned.
+ */
 int _islower(int c)
 {
-
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
 }
