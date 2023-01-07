623a75b087642dc9c702f7ccab842179e6ad2c13
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-07 02:51:47 +0100

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,8 +1,27 @@
 #include "main.h"
 
 /**
-  */
-unsigned int _strspn(char *s, char *accept);
-{
+ * _strspn - gets the length of a prefix substring.
+ * @s: string
+ * @accept: character
+ * Return:  number of bytes in the initial segment
+ * of s which consist only of bytes from accept.
+ */
 
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0, i = 0, j;
+
+	while (s[i] != 32)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+			}
+		}
+		i++;
+	}
+	return (c);
 }
