7a9c49b6cb94db90ffef871cc6a4209990278b4b
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-07 02:45:09 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,8 +1,19 @@
 #include "main.h"
 
 /**
-  */
+ * _strlen - returns the length of a string
+ * @s: param
+ * Return: lenght of s
+ */
+
 int _strlen(char *s)
 {
+	int c = 0;
 
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
 }
