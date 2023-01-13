0666b152923379dff43b51b910c5611a524bd5ab
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-13 13:05:20 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-		ptr = malloc(new_size);
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
