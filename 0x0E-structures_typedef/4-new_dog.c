9235c912bd75953f03de89ac76034bda8a65aa78
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-17 04:31:16 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,3 +46,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (pdog);
 }
+/**
+ * _strlen - returns the length of a string
+ * @s: param
+ * Return: lenght of s
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
