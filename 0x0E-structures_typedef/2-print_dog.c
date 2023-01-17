fd937dedd05c1638b19664599b490d615b423f8a
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-16 18:29:11 -0800

    dd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		(d->name == NULL ? printf("(nil)\n") : printf("Name: %s\n", d->name));
-		(d->age == 0.0 ? printf("0\n") : printf("Age: %f\n", d->age));
-		(d->owner == NULL ? printf("(nil)\n") : printf("Owner: %s\n", d->owner));
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
