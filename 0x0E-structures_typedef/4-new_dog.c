3d79be46a31efd12c7a1c6cd8e4d453a6d150b8d
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-01-17 04:32:10 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -63,3 +63,22 @@ int _strlen(char *s)
 
 	return (c);
 }
+/**
+ * _strcpy - copies the string pointed to by src.
+ * @dest:destination
+ * @src: source
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
