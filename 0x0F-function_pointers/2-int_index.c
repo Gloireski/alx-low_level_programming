79e3181cd8fce361b867bcb103afa576e1984853
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-19 17:27:27 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - ...
+  * @array: ...
+  * @size: ...
+  * @cmp: ...
+  *
+  * Return: int.
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]) == 0)
+			{
+				return (i);
+			}
+			i++;
+		}
+	}
+	else if (size <= 0)
+		return (-1);
+	return (-1);
+}
