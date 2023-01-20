aff779ed91d2262f896afc84ed13182ef98af6b9
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-19 18:27:41 -0800

    init cal

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - ...
+ * @s: ...
+ *
+ * Return: integer.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * op_add - ..
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - ..
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - ..
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - ...
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - ...
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
