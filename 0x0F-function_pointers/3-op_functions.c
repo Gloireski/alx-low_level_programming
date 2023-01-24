0a36eb96b804c04c7a6d03dc6ccc3f7bbaf3eaaa
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-23 16:00:46 -0800

    ll

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,6 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * op_add - ..
  * @a: ...
@@ -47,7 +50,10 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
-		return (0);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -62,6 +68,9 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
-		return (0);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
