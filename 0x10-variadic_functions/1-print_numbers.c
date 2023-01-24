3c531bfa53d2c880c1b62b1b4a87b48617a0524e
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-24 08:14:34 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,10 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d", va_arg(ap, int));
 			if (!(i == n - 1))
-				printf("%s", *separator);
+				printf("%s", separator);
 		}
 		va_end(ap);
-		return (s);
 	}
-	return (0);
 }
