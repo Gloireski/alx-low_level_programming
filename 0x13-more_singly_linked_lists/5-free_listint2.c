9435f42c02d7b9cb6a3b61e72fe77f5f19fbf270
Gloireski <belemgloirebekoutou8@gmail.com>
2023-01-31 18:06:26 -0800

    freeé

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_listint - Frees a linked list.
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: Nothing
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+	}
+	else
+	{
+		return;
+	}
+	free(*head);
+	head = 0;
+}
