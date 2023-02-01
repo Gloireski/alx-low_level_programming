beccbe0e4b525f1cb7f2d0ff2a3750b8a09ecb35
Gloireski <belemgloirebekoutou8@gmail.com>
2023-02-01 06:34:24 -0800

    fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,10 +12,13 @@ size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	if (h != NULL)
 	{
-		h = h->next;
-		count++;
+		while (h)
+		{
+			h = h->next;
+			count++;
+		}
 	}
 
 	return (count);
