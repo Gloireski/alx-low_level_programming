ae58cf01a166ab976f61945fce92cb4185ef6302
Gloireski <belemgloirebekoutou8@gmail.com>
2023-02-01 08:08:41 -0800

    

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		while (head && index != count)
 		{
 			head = head->next;
-			count ++;
+			count++;
 		}
 	}
 	if (count == index)
