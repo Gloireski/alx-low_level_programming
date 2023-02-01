ab7ca54d99cd6d483acaff7e3beb91b6fd21fd2f
Gloireski <belemgloirebekoutou8@gmail.com>
2023-02-01 07:51:53 -0800

    init ge_node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index -  returns the nth node of a listint_t linked list.
+  * @head: headnode pointer.
+  * @index: is the index of the node, starting at 0.
+  *
+  * Return: listint_t.
+  */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (head)
+	{
+		while (head && index != count)
+		{
+			head = head->next;
+		}
+	}
+	if (count == index)
+		return (head);
+	else
+		return (NULL);
+}
+
