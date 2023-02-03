e6f4f4df67e8aa667a41e89a84c0413eee93d868
Gloireski <belemgloirebekoutou8@gmail.com>
2023-02-02 17:46:11 -0800

    ni,

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,8 +13,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int len = 0, sum = 0, c = 0;
 
 	if (b == NULL)
-		return(0);
-
+		return (0);
+	len = _strlen(b);
+	while (len--)
+	{
+		if (b[len] != 48 && b[len] != 49)
+			return (0);
+		if (b[len] == 49)
+			sum += 1 << c;
+		c++;
+	}
+	return (sum);
 
 }
 
