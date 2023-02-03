7d417012fc9ecc89d4950ac540063f9e0ce2d205
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-02-03 02:29:55 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,3 +17,21 @@ unsigned int binary_to_uint(const char *b)
 
 
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: param
+ * Return: lenght of s
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
