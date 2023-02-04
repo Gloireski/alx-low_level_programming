7ecc429bac81fa751637e132fa161bc470fbac48
Gloireski <belemgloirebekoutou8@gmail.com>
2023-02-03 17:42:11 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,23 +12,23 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int c = 0;
-	int b;
 
 	if (index > n)
 		return (-1);
-	while (c != index)
+	while (n)
 	{
-		if (n < 1)
-			break;
+		if (c == index)
+		{
+			if (n & 1)
+				return (1);
+			else
+				return (0);
+		}
 		n = n >> 1;
 		c++;
 	}
-	if (c == index)
-	{
-		if (n & 1)
-			b = 1;
-		else
-			b = 0;
-	}
-	return (b);
+
+	if (index > c && index < 63)
+		return (0);
+	return (-1);
 }
