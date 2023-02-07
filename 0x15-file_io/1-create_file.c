b37286d7cd6e0ba1e71065c898b998cc0e325ddc
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-02-07 19:36:15 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,3 +25,18 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
