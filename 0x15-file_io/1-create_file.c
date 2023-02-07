4e5aa211bc75a0b759c96a41353b91a85fc6288f
Gloireski <belemgloirebekoutou8@gmail.com>
2023-02-07 10:34:41 -0800

    task2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY, 600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, nbytes);
+	if (text_content)
+		write(fd, text_content, nbytes);
 
 	close(fd);
 	return (1);
