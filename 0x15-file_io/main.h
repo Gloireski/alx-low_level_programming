6822e6f34276de869d6d244a0c24e0da1973ac23
Gloireski <belemgloirebekoutou8@gmail.com>
2023-02-07 18:05:49 -0800

    task4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,11 +4,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 
 #endif
