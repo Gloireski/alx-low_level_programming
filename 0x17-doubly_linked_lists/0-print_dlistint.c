bfad1d865b75e8d3ae3d03217188f7526731e216
Gloireski <belemgloirebekoutou8@gmail.com>
2023-03-16 15:53:50 -0700

    lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint -  prints all the elements of a dlistint_t list.
+ * @h: param
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+}
