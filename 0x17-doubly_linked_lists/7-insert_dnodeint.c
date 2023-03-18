5925afec00a02be7153dfb1cf244dd6051d654c5
Gloireski <belemgloirebekoutou8@gmail.com>
2023-03-17 18:00:07 -0700

    task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,15 +13,18 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int c = 0;
+	unsigned int c = 0, taille = 0;
 	dlistint_t *new = NULL, *tmp = NULL;
 
 	if (h == NULL)
 		return (NULL);
+	taille = dlistint_len(*h);
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
+	else if (taille == idx)
+		return (add_dnodeint_end(h, n));
 	tmp = *h;
 	while (tmp && idx != c)
 	{
@@ -47,4 +50,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp->prev->next = new;
 	}
 	return (new);
+}
+
+/**
+  * dlistint_len - Counts the number of elements in a doubly linked list
+  * @h: The double linked list to count
+  *
+  * Return: Number of elements in the doubly linked list
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int lenght = 0;
+
+	while (h != NULL)
+	{
+		++lenght;
+		h = h->next;
+	}
+
+	return (lenght);
 }
