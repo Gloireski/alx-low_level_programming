c40efb1348c1dddd17f3637ee5a2bc6669dbad57
Gloireski <belemgloirebekoutou8@gmail.com>
2023-11-03 04:47:38 -0700

    task 0 fixed

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: is the hash table you want to look into.
+ * @key:  is the key you are looking for
+ *
+ * Return: value associated with the el, or NULL not found
+ */
+
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+}
