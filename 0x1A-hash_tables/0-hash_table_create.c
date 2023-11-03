7ccefef7a704372ca557e975eed958718f1d6da4
Gloireski <belemgloirebekoutou8@gmail.com>
2023-11-03 03:51:47 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,19 +11,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+	hash_table_t *tab;
+	unsigned long int idx = 0;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (size <= 0)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
 
-	return (ht);
+	tab = malloc(sizeof(hash_table_t));
+	if (tab == NULL)
+		return (NULL);
+	tab->size = size;
+	tab->array = malloc(sizeof(hash_node_t *) * size);
+	if (tab->array == NULL)
+	{
+		free(tab->array);
+		return (NULL);
+	}
+
+	while (idx < size)
+	{
+		tab->array[idx] = NULL;
+		idx++;
+	}
+	return (tab);
 }
